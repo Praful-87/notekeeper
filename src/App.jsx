d4c93a5b0704027664d31024ed737557ce4d1306
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2023-09-23 09:41:26 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import "./App.css";
-import { GridItem, Input, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Divider,
+  GridItem,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  SimpleGrid,
+  HStack,
+  Box,
+  Image,
+} from "@chakra-ui/react";
 import Note from "./components/Note";
-
+import { SearchIcon } from "@chakra-ui/icons";
 function App() {
   return (
     <>
-      <VStack>
-        <Input maxW="500px" />
-      </VStack>
+      <HStack p="4">
+        <Box>
+          <Image src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" />
+        </Box>
+        <InputGroup maxW="500px">
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.300" />
+          </InputLeftElement>
+          <Input type="text" placeholder="Search for note..." />
+        </InputGroup>
+      </HStack>
+      <Divider />
       <SimpleGrid>
         <GridItem>
           <Note />
