34d277800ce598911b9315cb79a428365ffa0320
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2023-09-23 10:01:44 +0530

    add note aded

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import {
   HStack,
   Box,
   Image,
+  Text,
+  Flex,
+  Center,
 } from "@chakra-ui/react";
 import Note from "./components/Note";
 import { SearchIcon } from "@chakra-ui/icons";
@@ -17,10 +20,11 @@ function App() {
   return (
     <>
       <HStack p="4">
-        <Box>
+        <Flex align="center">
           <Image src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" />
-        </Box>
-        <InputGroup maxW="500px">
+          <Text>NoteKeeper</Text>
+        </Flex>
+        <InputGroup maxW="500px" ml={["20px", "40px", "300px"]}>
           <InputLeftElement pointerEvents="none">
             <SearchIcon color="gray.300" />
           </InputLeftElement>
@@ -28,6 +32,23 @@ function App() {
         </InputGroup>
       </HStack>
       <Divider />
+      <Center pos="relative">
+        <Center
+          mt="4"
+          rounded="md"
+          w="400px"
+          border="1px"
+          borderColor={"gray.300"}
+          h="40px"
+          fontSize="16px"
+        >
+          Add Note
+        </Center>
+        <Box pos={"absolute"} top="calc(100% + 10px)" shadow={"xl"} p="4">
+          <Input variant="unstyled" placeholder="Title" />
+          <Input variant="unstyled" placeholder="Title" mt="4" />
+        </Box>
+      </Center>
       <SimpleGrid>
         <GridItem>
           <Note />
